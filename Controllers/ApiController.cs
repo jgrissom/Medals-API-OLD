fc78e9ca7d275770cf21c2c7b03f00b1d000f5cc
jgrissom <jgrissom91@gmail.com>
2021-09-02 14:11:26 -0500

    [1.06] http get (all)

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Medals_API.Models;
+
+namespace Medals_API.Controllers
+{
+    [ApiController, Route("[controller]/country")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+        private DataContext _dataContext;
+
+        public ApiController(ILogger<ApiController> logger, DataContext db)
+        {
+            _dataContext = db;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<Country> Get()
+        {
+            return _dataContext.Countries;
+        }
+    }
+}
