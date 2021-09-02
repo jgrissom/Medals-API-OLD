d752063a72231fdb955e3183ee2af8797980f38c
jgrissom <jgrissom91@gmail.com>
2021-09-02 14:21:06 -0500

    [1.12] Swaggerdoc in release

--- Startup.cs
+++ Startup.cs
@@ -48,9 +48,10 @@ namespace Medals_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Medals_API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Medals API v1"));
 
             app.UseHttpsRedirection();
 
