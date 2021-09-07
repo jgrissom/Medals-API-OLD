485bd40f21e9fed26a93af0fefe8cdddb23a1903
jgrissom <jgrissom91@gmail.com>
2021-09-06 22:25:09 -0500

    [3.01] Async post, delete, patch methods
    - SignalR requires asynchronous api methods

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Medals_API.Models;
 using Swashbuckle.AspNetCore.Annotations;
 using Microsoft.AspNetCore.JsonPatch;
+using System.Threading.Tasks;
 
 namespace Medals_API.Controllers
 {
@@ -18,36 +19,52 @@ namespace Medals_API.Controllers
             _dataContext = db;
             _logger = logger;
         }
+
+        // http get entire collection
         [HttpGet, SwaggerOperation(summary: "return entire collection", null)]
         public IEnumerable<Country> Get()
         {
             return _dataContext.Countries;
         }
+
+        // http get specific member of collection
         [HttpGet("{id}"), SwaggerOperation(summary: "return specific member of collection", null)]
         public Country Get(int id)
         {
             return _dataContext.Countries.Find(id);
         }
+
+        // http post member to collection
         [HttpPost, SwaggerOperation(summary: "add member to collection", null), ProducesResponseType(typeof(Country), 201), SwaggerResponse(201, "Created")]
-        // add country
-        public Country Post([FromBody] Country country) => _dataContext.AddCountry(country);
+        public async Task<ActionResult<Country>> Post([FromBody] Country country) {
+            _dataContext.Add(country);
+            await _dataContext.SaveChangesAsync();
+            this.HttpContext.Response.StatusCode = 201;
+            return country;
+        } 
+
+        // http delete member from collection
         [HttpDelete("{id}"), SwaggerOperation(summary: "delete member from collection", null), ProducesResponseType(typeof(Country), 204), SwaggerResponse(204, "No Content")]
-        public ActionResult Delete(int id){
-            Country country = _dataContext.Countries.Find(id);
+        public async Task<ActionResult> Delete(int id){
+            Country country = await _dataContext.Countries.FindAsync(id);
             if (country == null){
                 return NotFound();
             }
-            _dataContext.DeleteCountry(country);
+            _dataContext.Remove(country);
+            await _dataContext.SaveChangesAsync();
             return NoContent();
         } 
+
+        // http patch member of collection
         [HttpPatch("{id}"), SwaggerOperation(summary: "update member from collection", null), ProducesResponseType(typeof(Country), 204), SwaggerResponse(204, "No Content")]
         // update country (specific fields)
-        public ActionResult Patch(int id, [FromBody]JsonPatchDocument<Country> patch){
-            Country country = _dataContext.Countries.Find(id);
+        public async Task<ActionResult> Patch(int id, [FromBody]JsonPatchDocument<Country> patch){
+            Country country = await _dataContext.Countries.FindAsync(id);
             if (country == null){
                 return NotFound();
             }
-            _dataContext.PatchCountry(id, patch);
+            patch.ApplyTo(country);
+            await  _dataContext.SaveChangesAsync();
             return NoContent();
         }
     }
