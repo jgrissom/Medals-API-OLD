8621e286f63af90ee25bea61700b39d039a45e93
jgrissom <jgrissom91@gmail.com>
2021-09-26 23:19:56 -0500

    [4.05] Sql Server Identity db

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Medals_API.Models;
 using Medals_API.Hubs;
+using Medals_API.Authentication;
 
 namespace Medals_API
 {
@@ -42,6 +36,7 @@ namespace Medals_API
                     });
             });
             services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultSQLiteConnection")));
+            services.AddDbContext<IdentityContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultSQLServerConnection")));
             services.AddControllers().AddNewtonsoftJson();
             services.AddSignalR();
             services.AddSwaggerGen(c =>
