4fc1bfec4e67731eb83dcfe95e39be0120415967
jgrissom <jgrissom91@gmail.com>
2023-09-11 22:56:21 -0500

    [1.06] Migrations
    dotnet ef migrations add initSQLite
    dotnet ef database update

---
+++ Migrations/20230912035557_initSQLite.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Medals_Api.Migrations
+{
+    public partial class initSQLite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Gold = table.Column<int>(type: "INTEGER", nullable: false),
+                    Silver = table.Column<int>(type: "INTEGER", nullable: false),
+                    Bronze = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
