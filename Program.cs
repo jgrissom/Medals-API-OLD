848c245cf7fd4efce914762bab59ea2b302c7ebc
jgrissom <jgrissom91@gmail.com>
2023-09-11 23:09:50 -0500

    [1.13] Swaggerdoc in release

--- Program.cs
+++ Program.cs
@@ -31,11 +31,11 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
